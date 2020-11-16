8bbec99eec8d9324eb535c58aa25ee166afb5e91
Sounak Ghosh <sounaktheghosh@gmail.com>
2020-11-16 00:32:43 -0500

    api loaded

--- sketch.js
+++ sketch.js
@@ -1,13 +1,25 @@
-let slider;
+var rigveda;
+var api_url = 'https://sheetlabs.com/IND/rv?sungfor=Agni';
 
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(640, 480);
     
-    slider = createSlider(0,255,200)
+    var input = createInput('animal');
+    var button = createButton('submit');
 }
 
+async function gotData() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log(data);
+}
+
+gotData();
+
 function draw() {
-  background(220);
-    
-    
+    background(220);
+//    if (rigveda) {
+//        var 
+//    }
+//    
 }
