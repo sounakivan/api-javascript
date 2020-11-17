6fdaa50735734c7771d8df6e765b2c41ed7ae602
Sounak Ghosh <sounaktheghosh@gmail.com>
2020-11-17 16:47:47 -0500

    mandals loading, suktas to update

--- sketch.js
+++ sketch.js
@@ -1,25 +1,90 @@
-var rigveda;
-var api_url = 'https://sheetlabs.com/IND/rv?sungfor=Agni';
+rigVeda = [];
+suktas = [];
+
+let init_url = 'https://sheetlabs.com/IND/rv?';
+let mandal_url = '';
+let sukta_url = '';
+let sungfor_url = '';
+
+let mandal;
+let sukta;
+let diety;
+let button;
+
+let api_url = init_url + mandal_url + sukta_url + sungfor_url;
+console.log(api_url)
 
 function setup() {
-    createCanvas(640, 480);
+    createCanvas(640, 480, WEBGL);
+     
+    mandal = createSelect();
+    mandal.position(50,500);
+    mandal.size(100,25);
+    for (i = 0; i < 10; i++) {
+        mandal.option(i+1);
+    }
     
-    var input = createInput('animal');
-    var button = createButton('submit');
+    mandal.changed(getMandalData);
+
+    sukta = createSelect();
+    sukta.position(200,500);
+    sukta.size(100,25);
+    
+    diety = createSelect();
+    diety.position(350,500);
+    diety.size(100,25);
+    
+    button = createButton('generate');
+    button.position(525,500);
+    button.size(75,45);
+    
+    //button.mousePressed(getVeda);
 }
 
-async function gotData() {
-    const response = await fetch(api_url);
-    const data = await response.json();
-    console.log(data);
+function getMandalData() {
+    //set mandal_num
+    let m_num = mandal.value();
+    mandal_url = 'mandal=' + m_num;
+    //console.log(mandal_url);
+    
+    api_url = init_url + mandal_url + sukta_url + sungfor_url;
+    console.log(api_url);
+    
+    fetch(api_url)
+    .then(response => response.json())
+    .then(mandalData => loadMandal(mandalData));
 }
 
-gotData();
+function loadMandal(mandalData) {
+    rigVeda = mandalData;
+    console.log(rigVeda);
+    
+    //clear suktas array
+    if (suktas.length != 0) {
+        suktas.length = 0
+    }
+    console.log(suktas.length);
+    
+    //update suktas
+    for (i = 0; i < rigVeda.length; i++) {
+        suktas.push(rigVeda[i].sukta);
+    }
+    let suktaNums = [...new Set(suktas)];
+    console.log(suktas.length);
+    //console.log(suktaNums);
+    
+    for (j = 0; j < suktaNums.length; j++) {
+        sukta.option(suktaNums[j]);
+    }
+    console.log(sukta.option.length);
+
+}
+
+//gotData();
+//setInterval(gotData, 2000);
 
 function draw() {
     background(220);
-//    if (rigveda) {
-//        var 
-//    }
-//    
+    //console.log(rigVeda.length);
+   
 }
